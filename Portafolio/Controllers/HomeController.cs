f678d7a4e1eb3d59671643243391f57994b0ad4e
Santi <sweihmuller01@gmail.com>
2025-08-18 16:47:11 -0300

    applying single-responsibility principle

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,59 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 
 namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos _repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos) 
         {
             _logger = logger;
+            _repositorioProyectos = repositorioProyectos; 
+            
         }
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(2).ToList();
+            var proyectos = _repositorioProyectos.ObtenerProyectos().Take(2).ToList();
             var modelo = new HomeIndexViewModel() { proyectos = proyectos };
             return View(modelo);
         }
 
-        private List<Proyecto> ObtenerProyectos()
-        {
-            return new List<Proyecto>()
-            {
-                new Proyecto
-                {
-                    titulo = "Girar el dado",
-                    descripcion ="Aplicación en Flutter donde el usuario aprieta el dado y aparece una de las caras del dado",
-                    link = "https://github.com/sweihmuller/Roll-dice",
-                    imagenURL = "imagenes/girarDado.png"
-                },
-                new Proyecto
-                {
-                    titulo = "Quiz",
-                    descripcion ="Aplicación en Flutter donde el usuario responde unas preguntas con opción múltiple.",
-                    link = "https://github.com/sweihmuller/quiz_app",
-                    imagenURL = "imagenes/quiz.png"
-                },
-                new Proyecto
-                {
-                    titulo = "Gestión de gastos",
-                    descripcion ="Aplicación en Flutter con estructura inicial para seguimiento de gastos personales",
-                    link = "https://github.com/sweihmuller/Expense-Tracker",
-                    imagenURL = "imagenes/gestionGastos.png"
-                },
-                new Proyecto
-                {
-                    titulo = "Receta de comida",
-                    descripcion ="Aplicación en Flutter que muestra la recetas de distintas comidas, mostrarlas según condiciones y elegir tus favoritas.",
-                    link = "https://github.com/sweihmuller/Expense-Tracker",
-                    imagenURL = "imagenes/recetaComidas.png"
-                },
-            };
-        }
+       
 
         public IActionResult Privacy()
         {
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    titulo = "Girar el dado",
+                    descripcion ="Aplicación en Flutter donde el usuario aprieta el dado y aparece una de las caras del dado",
+                    link = "https://github.com/sweihmuller/Roll-dice",
+                    imagenURL = "imagenes/girarDado.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Quiz",
+                    descripcion ="Aplicación en Flutter donde el usuario responde unas preguntas con opción múltiple.",
+                    link = "https://github.com/sweihmuller/quiz_app",
+                    imagenURL = "imagenes/quiz.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Gestión de gastos",
+                    descripcion ="Aplicación en Flutter con estructura inicial para seguimiento de gastos personales",
+                    link = "https://github.com/sweihmuller/Expense-Tracker",
+                    imagenURL = "imagenes/gestionGastos.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Receta de comida",
+                    descripcion ="Aplicación en Flutter que muestra la recetas de distintas comidas, mostrarlas según condiciones y elegir tus favoritas.",
+                    link = "https://github.com/sweihmuller/Expense-Tracker",
+                    imagenURL = "imagenes/recetaComidas.png"
+                },
+            };
+        }
+    }
+}
