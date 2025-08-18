4e41ba00fdd1b4213d4d3a0cea885069fb430c4f
Santi <sweihmuller01@gmail.com>
2025-08-18 16:22:03 -0300

    Adding projects

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,11 +15,44 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            Persona persona = new Persona
+            var proyectos = ObtenerProyectos().Take(2).ToList();
+            var modelo = new HomeIndexViewModel() { proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
             {
-                Nombre = "Santiago Weihmüller",
+                new Proyecto
+                {
+                    titulo = "Girar el dado",
+                    descripcion ="Aplicación en Flutter donde el usuario aprieta el dado y aparece una de las caras del dado",
+                    link = "https://github.com/sweihmuller/Roll-dice",
+                    imagenURL = "imagenes/girarDado.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Quiz",
+                    descripcion ="Aplicación en Flutter donde el usuario responde unas preguntas con opción múltiple.",
+                    link = "https://github.com/sweihmuller/quiz_app",
+                    imagenURL = "imagenes/quiz.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Gestión de gastos",
+                    descripcion ="Aplicación en Flutter con estructura inicial para seguimiento de gastos personales",
+                    link = "https://github.com/sweihmuller/Expense-Tracker",
+                    imagenURL = "imagenes/gestionGastos.png"
+                },
+                new Proyecto
+                {
+                    titulo = "Receta de comida",
+                    descripcion ="Aplicación en Flutter que muestra la recetas de distintas comidas, mostrarlas según condiciones y elegir tus favoritas.",
+                    link = "https://github.com/sweihmuller/Expense-Tracker",
+                    imagenURL = "imagenes/recetaComidas.png"
+                },
             };
-            return View("Index", persona);
         }
 
         public IActionResult Privacy()
---
+++ Portafolio/Models/Proyecto.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class Proyecto
+    {
+        public string titulo { get; set; }
+        public string descripcion {  get; set; }
+        public string imagenURL { get; set; }
+        public string link {  get; set; }
+    }
+}
