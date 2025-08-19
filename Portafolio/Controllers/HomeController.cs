f2e5f472f8ee41ae6231a8c80cf79c168256f489
Santi <sweihmuller01@gmail.com>
2025-08-18 21:10:31 -0300

    Create form contact

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -31,6 +31,20 @@ namespace Portafolio.Controllers
             return View(proyectos);
         }
 
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+        }
+        public IActionResult Gracias()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Portafolio/Models/ContactoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class ContactoViewModel
+    {
+        public String nombre {  get; set; }
+        public String email { get; set; }
+        public String mensaje { get; set; }
+
+    }
+}
